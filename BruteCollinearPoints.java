b861ed088da3ca6ce1bdacebaca9fe1da86c4350
Mengyu Li <limengyuw@gmail.com>
2018-11-30 10:14:54 +1100

    Finish BruteCollinearPoints class.

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,22 +1,15 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 public class BruteCollinearPoints {
 
     private int count;
     private LineSegment[] lineArrayList;
 
-    private class Node
-    {
-        LineSegment value;
-        Node next;
-    }
+
 
     public BruteCollinearPoints(Point[] points)
     {
-        Node Head;
-        Node Tail;
-        Head = new Node();
-        Tail = Head;
-        count = 0;
+        ArrayList<LineSegment> lines = new ArrayList<>();
         if (points == null)
         {
             throw new java.lang.IllegalArgumentException();
@@ -29,20 +22,28 @@ public class BruteCollinearPoints {
             }
             for (int j = i+1; j < points.length; j++)
             {
+                if (points[j] == null)
+                {
+                    throw new java.lang.IllegalArgumentException();
+                }
                 if (points[i].compareTo(points[j]) == 0)
                 {
                     throw new java.lang.IllegalArgumentException();
                 }
             }
         }
-        for (Point p: points)
+        for (int i = 0; i < points.length; i++)
         {
-            for (Point q: points)
+            for (int j = i + 1; j < points.length; j++)
             {
-                for (Point r: points)
+                for (int k = j + 1; k < points.length; k++)
                 {
-                    for (Point s: points)
+                    for (int l = k + 1; l < points.length; l++)
                     {
+                        Point p = points[i];
+                        Point q = points[j];
+                        Point r = points[k];
+                        Point s = points[l];
                         if (p.slopeTo(q) == p.slopeTo(r) && p.slopeTo(r) == p.slopeTo(s))
                         {
                             Point[] pointArray = new Point[4];
@@ -51,9 +52,7 @@ public class BruteCollinearPoints {
                             pointArray[2] = r;
                             pointArray[3] = s;
                             Arrays.sort(pointArray);
-                            Tail.value = new LineSegment(pointArray[0], pointArray[3]);
-                            Tail.next = new Node();
-                            Tail = Tail.next;
+                            lines.add(new LineSegment(pointArray[0], pointArray[3]));
                             count++;
 
                         }
@@ -62,12 +61,9 @@ public class BruteCollinearPoints {
             }
         }
         lineArrayList = new LineSegment[count];
-        Node current = Head;
         for (int i = 0; i < count; i++)
         {
-            lineArrayList[i] = current.value;
-            current = current.next;
-
+            lineArrayList[i] = lines.get(i);
         }
 
     }
@@ -79,6 +75,16 @@ public class BruteCollinearPoints {
 
     public LineSegment[] segments()
     {
-        return lineArrayList;
+        LineSegment[] to_return = new LineSegment[count];
+        for (int i = 0; i < count; i++)
+        {
+            to_return[i] = lineArrayList[i];
+        }
+        return to_return;
+    }
+
+    public static void main(String[] args)
+    {
+
     }
 }
